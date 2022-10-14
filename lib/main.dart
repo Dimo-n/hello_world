1247bced6d84df1e5ae8aa6725aa26fb926a0ccd
Dimo-n <dimon.chmara@gmail.com>
2022-10-14 21:10:55 +0300

    New project hello_world

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Material(
+        color: Color.fromARGB(255, 19, 41, 186),
+        child: Center(
+          child: Text(
+            'Слава Україні!',
+            style: TextStyle(fontSize: 46, color: Colors.yellowAccent),)
+              ),
+        )
+    ),
+  );
+}
+
+
